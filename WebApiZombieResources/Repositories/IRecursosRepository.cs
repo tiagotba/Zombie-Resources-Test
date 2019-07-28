8aef30cd472b11482d838bc62b0984fc99eed72c
tiagotba <osbentoalves@gmail.com>
2019-07-28 10:56:05 -0300

    começo da criação dos repositories

---
+++ WebApiZombieResources/Repositories/IRecursosRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiZombieResources.Models;
+
+namespace WebApiZombieResources.Repositories
+{
+    interface IRecursosRepository
+    {
+        IEnumerable<Recursos> GetRecursos();
+        Recursos GetRecursos(int id);
+        void CreateRecurso(Recursos recursos);
+        void UpdateRecurso(Recursos recursos, int id);
+        void DeleRecurso(int id);
+    }
+}
